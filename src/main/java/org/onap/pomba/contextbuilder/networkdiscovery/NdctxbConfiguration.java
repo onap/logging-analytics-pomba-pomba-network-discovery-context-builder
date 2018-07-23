13b9f611d7bfeb295618929d4e76b7de5535612f
Tait,Trevor(rt0435) <rtait@amdocs.com>
2018-07-23 16:49:03 -0400

    Initial Network Discovery Context Builder code
    Issue-ID: LOG-401
    Change-Id: Id7211386f5eb7f9de2706037fbaf336c636e906c
    Signed-off-by: Tait,Trevor(rt0435) <rtait@amdocs.com>

---
+++ src/main/java/org/onap/pomba/contextbuilder/networkdiscovery/NdctxbConfiguration.java
@@ -0,0 +1,81 @@
+/*
+ * ============LICENSE_START===================================================
+ * Copyright (c) 2018 Amdocs
+ * ============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================================
+ */
+package org.onap.pomba.contextbuilder.networkdiscovery;
+
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NdctxbConfiguration {
+
+    // Network Discover Configuration values
+
+    @Value("${nd.userId:admin}")
+    private String ndUserId;
+
+    @Value("${nd.password:admin}")
+    private String ndPassword;
+
+    // Service Decomposition Configuration values
+
+    @Value("${sd.host:127.0.0.1}")
+    private String host;
+
+    @Value("${sd.port}")
+    private String port;
+
+    @Value("${sd.httpProtocol}")
+    private String httpProtocol;
+
+    @Value("${sd.serviceInstancePath}")
+    private String serviceInstancePath;
+
+    @Value("${sd.userId:admin}")
+    private String sdUserId;
+
+    @Value("${sd.password:admin}")
+    private String sdPassword;
+
+    @Bean(name="sdBaseUrl")
+    public String getURL() {
+        String url = this.httpProtocol + "://" + this.host + ":" + this.port + this.serviceInstancePath;
+        return url;
+    }
+
+    @Bean(name="sdSeriveInstancePath")
+    public String getSeriveInstancePath() {
+        String serviceInstancePath = this.serviceInstancePath;
+        return serviceInstancePath;
+    }
+
+    @Bean(name="sdBasicAuthorization")
+    public String getSdBasicAuth() {
+        String auth = new String(this.sdUserId + ":" + this.sdPassword);
+        return ("Basic " + Base64.getEncoder().encodeToString(auth.getBytes()));
+    }
+
+    @Bean(name="ndBasicAuthorization")
+    public String getNdBasicAuth() {
+        String auth = this.ndUserId + ":" + this.ndPassword;
+        return ("Basic " + Base64.getEncoder().encodeToString(auth.getBytes()));
+    }
+
+}
---
+++ src/main/java/org/onap/pomba/contextbuilder/networkdiscovery/service/rs/RestServiceImpl.java
@@ -0,0 +1,84 @@
+/*
+ * ============LICENSE_START===================================================
+ * Copyright (c) 2018 Amdocs
+ * ============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================================
+ */
+package org.onap.pomba.contextbuilder.networkdiscovery.service.rs;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.onap.pomba.common.datatypes.ModelContext;
+import org.onap.pomba.contextbuilder.networkdiscovery.exception.DiscoveryException;
+import org.onap.pomba.contextbuilder.networkdiscovery.exception.ErrorMessage;
+
+import org.onap.pomba.contextbuilder.networkdiscovery.service.SpringService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Component
+public class RestServiceImpl implements RestService {
+    private static Logger log = LoggerFactory.getLogger(RestService.class);
+    private static final String EMPTY_JSON_OBJECT = "{}";
+    private static final String HEADER_REQUEST_ID = "X-ONAP-RequestID";
+
+    @Autowired
+    private SpringService service;
+
+    @Override
+    public Response getContext(HttpServletRequest req,
+                               String authorization,
+                               String partnerName,
+                               String requestId,
+                               String serviceInstanceId,
+                               String modelVersionId,
+                               String modelInvariantId) throws DiscoveryException {
+
+        // Do some validation on Http headers and URL parameters
+
+        if (requestId == null || requestId.isEmpty()) {
+               requestId = UUID.randomUUID().toString();
+               log.debug(HEADER_REQUEST_ID + " is missing; using newly generated value: " + requestId);
+        }
+
+        try {
+            ModelContext sdContext = service.getContext(req, partnerName, authorization, requestId, serviceInstanceId, modelVersionId, modelInvariantId);
+            if (sdContext == null) {
+                // Return empty JSON
+                return Response.ok().entity(EMPTY_JSON_OBJECT).build();
+            } else {
+                Gson gson = new GsonBuilder().create();
+                return Response.ok().entity(gson.toJson(sdContext)).build();
+            }
+        } catch (DiscoveryException x) {
+            log.error(ErrorMessage.CONTEXT_BUILDER_FAILED);
+            return Response.status(x.getHttpStatus()).entity(x.getMessage()).build();
+
+        } catch (Exception x) {
+            log.error(ErrorMessage.CONTEXT_BUILDER_FAILED, x);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(x.getMessage()).build();
+        }
+    }
+
+
+}
