f4d2e331ced628ace2b5ba12a7c0cd1ac315e62b
Tait,Trevor(rt0435) <rtait@amdocs.com>
2018-07-23 16:49:03 -0400

    Initial Network Discovery Context Builder code
    Issue-ID: LOG-401
    Change-Id: Id7211386f5eb7f9de2706037fbaf336c636e906c
    Signed-off-by: Tait,Trevor(rt0435) <rtait@amdocs.com>

---
+++ src/main/java/org/onap/pomba/contextbuilder/nd/exception/DiscoveryException.java
@@ -0,0 +1,49 @@
+/*
+ * ============LICENSE_START===================================================
+ * Copyright (c) 2018 Amdocs
+ * ============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================================
+ */
+package org.onap.pomba.contextbuilder.nd.exception;
+
+import javax.ws.rs.core.Response.Status;
+
+public class DiscoveryException extends Exception {
+
+    private static final long serialVersionUID = -4874149714911165454L;
+
+    private final Status httpStatus;
+
+    public DiscoveryException(String message) {
+        this(message, Status.INTERNAL_SERVER_ERROR);
+    }
+
+    public DiscoveryException(String message, Status httpStatus) {
+        super(message);
+        if (httpStatus == null) {
+            throw new NullPointerException("httpStatus");
+        }
+        this.httpStatus = httpStatus;
+    }
+
+    public DiscoveryException(String message, Exception cause) {
+        super(message, cause);
+        this.httpStatus = Status.INTERNAL_SERVER_ERROR;
+    }
+
+    public Status getHttpStatus() {
+        return this.httpStatus;
+    }
+
+}
---
+++ src/test/java/org/onap/pomba/contextbuilder/nd/unittest/service/NetworkDiscoveryContextBuilderTest.java
@@ -0,0 +1,112 @@
+/*
+ * ============LICENSE_START===================================================
+ * Copyright (c) 2018 Amdocs
+ * ============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================================
+ */
+
+package org.onap.pomba.contextbuilder.nd.unittest.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onap.pomba.contextbuilder.nd.service.rs.RestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@WebAppConfiguration
+@SpringBootTest
+public class NetworkDiscoveryContextBuilderTest {
+
+    private String authorization = "Basic "
+                                    + Base64.getEncoder()
+                                    .encodeToString(("admin" + ":" + "admin")
+                                    .getBytes(StandardCharsets.UTF_8));
+    private String partnerName = "POMBA";
+    private String transactionId = UUID.randomUUID().toString();
+    private String serviceInstanceId = "c6456519-6acf-4adb-997c-3c363dd4caaf";
+
+    HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+
+    @Autowired
+    RestService restService;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testVerifyNoAuthoriztion() throws Exception {
+        Response response = this
+                            .restService
+                            .getContext(httpServletRequest, null, partnerName, transactionId, serviceInstanceId, null, null);
+        assertTrue(response.getEntity().toString().contains("Missing Authorization: "));
+        assertTrue(response.getStatus() == Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    }
+
+    @Test
+    public void testVerifyBadAuthoriztion() throws Exception {
+        String authorization = "Basic "
+                + Base64.getEncoder()
+               .encodeToString(("Test" + ":" + "Fake")
+               .getBytes(StandardCharsets.UTF_8));
+        Response response = this
+                            .restService
+                            .getContext(httpServletRequest, authorization, partnerName, transactionId, serviceInstanceId, null, null);
+        assertEquals("Authorization Failed",response.getEntity().toString());
+        assertTrue(response.getStatus() == Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    }
+
+
+    @Test
+    public void testVerifyPartnerName() throws Exception {
+        Response response = this
+                            .restService
+                            .getContext(httpServletRequest, authorization, null, transactionId, serviceInstanceId, null, null);
+        assertTrue(response.getEntity().toString().contains("X-ONAP-PartnerName"));
+        assertTrue(response.getStatus() == Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    }
+
+    @Test
+    public void testServiceInstanceId() throws Exception {
+        Response response = this.restService.getContext(httpServletRequest, authorization, partnerName, transactionId, null, null, null);
+        assertTrue(response.getEntity().toString().contains("serviceInstanceId"));
+        assertTrue(response.getStatus() == Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    }
+
+}
