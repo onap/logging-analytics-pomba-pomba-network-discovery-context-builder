ee548a19ed79a279d0ba2b0e87bfcc64c4179e23
Phillip Leigh <phillip.leigh@amdocs.com>
2018-07-27 11:18:51 -0400

    Stitch btw NetworkDiscovery&ServiceDecomp& CtxAggr
    Issue-ID: LOG-599
    Change-Id: Ia96ee1f7e586f385a3dc9c4fe0c5ff3badfeeea3
    Signed-off-by: Phillip Leigh <phillip.leigh@amdocs.com>

--- src/test/java/org/onap/pomba/contextbuilder/networkdiscovery/test/NdctxbConfigurationTest.java
+++ src/test/java/org/onap/pomba/contextbuilder/networkdiscovery/test/NdctxbConfigurationTest.java
@@ -21,7 +21,6 @@ package org.onap.pomba.contextbuilder.networkdiscovery.test;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import org.junit.Test;
 import org.onap.pomba.contextbuilder.networkdiscovery.NdctxbConfiguration;
 
