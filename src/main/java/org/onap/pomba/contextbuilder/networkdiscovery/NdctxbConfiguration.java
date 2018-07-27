f144dda91d4a488fcc0f7843fbc771247f461b60
Phillip Leigh <phillip.leigh@amdocs.com>
2018-07-27 11:18:51 -0400

    Stitch btw NetworkDiscovery&ServiceDecomp& CtxAggr
    Issue-ID: LOG-599
    Change-Id: Ia96ee1f7e586f385a3dc9c4fe0c5ff3badfeeea3
    Signed-off-by: Phillip Leigh <phillip.leigh@amdocs.com>

---
+++ src/main/java/org/onap/pomba/contextbuilder/networkdiscovery/NdctxbConfiguration.java
@@ -0,0 +1,156 @@
+/*
+ * ============LICENSE_START===================================================
+ * Copyright (c) 2018 Amdocs
+ * ============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================================
+ */
+
+package org.onap.pomba.contextbuilder.networkdiscovery;
+
+import java.net.InetAddress;
+import java.util.Base64;
+
+import org.eclipse.jetty.util.security.Password;
+import org.onap.pomba.contextbuilder.networkdiscovery.exception.DiscoveryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NdctxbConfiguration {
+	private static Logger log = LoggerFactory.getLogger(NdctxbConfiguration.class);
+
+    // Network Discover Configuration values
+
+    @Value("${networkDiscoveryCtxBuilder.userId:admin}")
+    private String networkDiscoveryCtxBuilderUserId;
+
+    @Value("${networkDiscoveryCtxBuilder.password:admin}")
+    private String networkDiscoveryCtxBuilderPassword;
+
+    // Service Decomposition Configuration values
+
+    @Value("${serviceDecomposition.host:127.0.0.1}")
+    private String serviceDecompositionHost;
+
+    @Value("${serviceDecomposition.port}")
+    private String serviceDecompositionPort;
+
+    @Value("${serviceDecomposition.httpProtocol}")
+    private String serviceDecompositionHttpProtocol;
+
+    @Value("${serviceDecomposition.serviceInstancePath}")
+    private String serviceDecompositionServiceInstancePath;
+
+    @Value("${serviceDecomposition.userId:admin}")
+    private String serviceDecompositionUserId;
+
+    @Value("${serviceDecomposition.password:admin}")
+    private String serviceDecompositionPassword;
+
+    @Bean(name="serviceDecompositionBaseUrl")
+    public String getURL() {
+        String url = this.serviceDecompositionHttpProtocol + "://"
+                + this.serviceDecompositionHost + ":"
+                + this.serviceDecompositionPort
+                + this.serviceDecompositionServiceInstancePath;
+        return url;
+    }
+
+    @Bean(name="serviceDecompositionBasicAuthorization")
+    public String getSdBasicAuth() {
+        String auth = new String(this.serviceDecompositionUserId + ":" + Password.deobfuscate(this.serviceDecompositionPassword));
+        return ("Basic " + Base64.getEncoder().encodeToString(auth.getBytes()));
+    }
+
+    @Bean(name="networkDiscoveryCtxBuilderBasicAuthorization")
+    public String getNdBasicAuth() {
+        String auth = this.networkDiscoveryCtxBuilderUserId + ":" + Password.deobfuscate(this.networkDiscoveryCtxBuilderPassword);
+        return ("Basic " + Base64.getEncoder().encodeToString(auth.getBytes()));
+    }
+
+	/* Network Discovery related */
+	@Value("${networkDiscoveryMicroService.host}")
+	private String networkDiscoveryMicroServiceHost;
+
+	@Value("${networkDiscoveryMicroService.port}")
+	private String networkDiscoveryMicroServicePort;
+
+	@Value("${networkDiscoveryMicroService.httpProtocol}")
+	private String httpNetworkDiscoveryMicroServiceProtocol;
+
+	@Value("${networkDiscoveryMicroService.networkDiscoveryPath}")
+	private String networkDiscoveryMicroServicePath;
+
+	@Value("${networkDiscoveryMicroService.responseTimeOutInMilliseconds}")
+	private String networkDiscoveryResponseTimeOutInMilliseconds;
+
+	@Bean(name = "networkDiscoveryMicroServiceBaseUrl")
+	public String getNetworkDiscoveryURL() {
+		String url = this.httpNetworkDiscoveryMicroServiceProtocol + "://" + this.networkDiscoveryMicroServiceHost + ":"
+				+ this.networkDiscoveryMicroServicePort + this.networkDiscoveryMicroServicePath;
+		return url;
+	}
+
+	@Bean(name = "ndResponseTimeOutInMilliseconds")
+	public long getNdResponseTimeOutInMilliseconds() {
+		long timeoutV = Integer.parseUnsignedInt(this.networkDiscoveryResponseTimeOutInMilliseconds);
+		return timeoutV;
+	}
+
+	/* Network Discovery Context Builder related */
+	@Value("${server.port:8080}")
+	private int networkDiscoveryCtxBuilderPort;
+
+	@Value("${networkDiscoveryCtxBuilder.httpProtocol}")
+	private String httpNetworkDiscoveryCtxBuilderProtocol;
+
+	@Bean(name = "networkDiscoveryCtxBuilderPort")
+	public String getNetworkDiscoveryCtxBuilderPort() {
+		return Integer.toString(networkDiscoveryCtxBuilderPort);
+	}
+
+	@Bean(name = "networkDiscoveryCtxBuilderBaseUrl")
+	public String getNetworkDiscoveryCtxBuilderBaseUrl() throws DiscoveryException {
+		String url = null;
+		try {
+			String localIp = InetAddress.getLocalHost().getHostAddress();
+			url = this.httpNetworkDiscoveryCtxBuilderProtocol + "://" + localIp + ":"
+					+ getNetworkDiscoveryCtxBuilderPort();
+		} catch (Exception e) {
+			log.error("Unable to obtain localIp: " + e.getMessage());
+			throw new DiscoveryException(e.getMessage(), e);
+		}
+
+		// testing,
+
+		return url;
+	}
+
+	// Network Discover Configuration values
+	@Value("${networkDiscoveryMicroService.userId:admin}")
+	private String networkDiscoveryMicroServiceUserId;
+
+	@Value("${networkDiscoveryMicroService.password:admin}")
+	private String networkDiscoveryMicroServicePassword;
+
+	@Bean(name = "networkDiscoveryMicroServiceBasicAuthorization")
+	public String getNetworkDiscoveryMicroServiceBasicAuth() {
+		String auth = this.networkDiscoveryMicroServiceUserId + ":" + this.networkDiscoveryMicroServicePassword;
+		return ("Basic " + Base64.getEncoder().encodeToString(auth.getBytes()));
+	}
+    
+}
---
+++ src/main/java/org/onap/pomba/contextbuilder/networkdiscovery/service/rs/RestServiceImpl.java
@@ -0,0 +1,127 @@
+/*
+ * ============LICENSE_START===================================================
+ * Copyright (c) 2018 Amdocs
+ * ============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=====================================================
+ */
+package org.onap.pomba.contextbuilder.networkdiscovery.service.rs;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.onap.pomba.common.datatypes.ModelContext;
+import org.onap.pomba.contextbuilder.networkdiscovery.exception.DiscoveryException;
+import org.onap.pomba.contextbuilder.networkdiscovery.exception.ErrorMessage;
+import org.onap.pomba.contextbuilder.networkdiscovery.service.SpringService;
+import org.onap.sdnc.apps.pomba.networkdiscovery.datamodel.NetworkDiscoveryNotification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+@Component
+public class RestServiceImpl implements RestService {
+    private static Logger log = LoggerFactory.getLogger(RestService.class);
+    private static final String EMPTY_JSON_OBJECT = "{}";
+    private static final String HEADER_REQUEST_ID = "X-ONAP-RequestID";
+
+    @Autowired
+    private SpringService service;
+
+	@Autowired
+	private String networkDiscoveryMicroServiceBasicAuthorization;
+
+    @Override
+    public Response getContext(HttpServletRequest req,
+                               String authorization,
+                               String partnerName,
+                               String requestId,
+                               String serviceInstanceId,
+                               String modelVersionId,
+                               String modelInvariantId) throws DiscoveryException {
+
+        // Do some validation on Http headers and URL parameters
+
+        if (requestId == null || requestId.isEmpty()) {
+               requestId = UUID.randomUUID().toString();
+               log.debug(HEADER_REQUEST_ID + " is missing; using newly generated value: " + requestId);
+        }
+
+        try {
+            ModelContext sdContext = service.getContext(req, partnerName, authorization, requestId, serviceInstanceId, modelVersionId, modelInvariantId);
+            if (sdContext == null) {
+                // Return empty JSON
+                return Response.ok().entity(EMPTY_JSON_OBJECT).build();
+            } else {
+                Gson gson = new GsonBuilder().create();
+                return Response.ok().entity(gson.toJson(sdContext)).build();
+            }
+        } catch (DiscoveryException x) {
+            log.error(ErrorMessage.CONTEXT_BUILDER_FAILED);
+            return Response.status(x.getHttpStatus()).entity(x.getMessage()).build();
+
+        } catch (Exception x) {
+            log.error(ErrorMessage.CONTEXT_BUILDER_FAILED, x);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(x.getMessage()).build();
+        }
+    }
+
+	@Override
+	public Response networkDiscoveryNotification(NetworkDiscoveryNotification notification, String authorization)
+			throws DiscoveryException {
+		Gson gson = new Gson();
+		String jsonInString = gson.toJson(notification);
+		log.info("Receiving networkDiscoveryNotification ... " + jsonInString);
+
+		try {
+			// The calling server (network discovery microService)
+			// doesn't check the response.
+			validateBasicAuth(authorization);
+			this.service.networkDiscoveryNotification(notification, authorization);
+			return Response.ok("Ack").build();
+
+		} catch (DiscoveryException x) {
+			log.error("context builder failed", x);
+			return Response.status(x.getHttpStatus()).entity(x.getMessage()).build();
+
+		} catch (Exception x) {
+			log.error("context builder failed", x);
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(x.getMessage()).build();
+		}
+	}
+
+	/**
+	 * Validates the Basic authorization header as admin:admin.
+	 *
+	 * @throws DiscoveryException
+	 *             if there is missing parameter
+	 */
+	private void validateBasicAuth(String authorization) throws DiscoveryException {
+		if (authorization != null && !authorization.trim().isEmpty()) {
+			if (!authorization.equals(networkDiscoveryMicroServiceBasicAuthorization)) {
+				throw new DiscoveryException("Authorization Failed!", Status.UNAUTHORIZED);
+			}
+		} else {
+			// authentication is null or empty
+			throw new DiscoveryException(
+					"Missing Authorization: " + (authorization == null ? "null" : authorization.toString()),
+					Status.UNAUTHORIZED);
+		}
+	}
+}
