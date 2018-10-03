86ea80127912cb8b1134b22c502bf9950e197d65
Shane Daniel <shane.daniel@amdocs.com>
2018-10-03 16:08:08 -0400

    Fix missing vserver name attribute
    Issue-ID: SDNC-470
    Change-Id: Ieb605efdd128251ed057942fff1239ad93c4fdf1
    Signed-off-by: Shane Daniel <shane.daniel@amdocs.com>

--- src/main/java/org/onap/pomba/contextbuilder/networkdiscovery/service/SpringServiceImpl.java
+++ src/main/java/org/onap/pomba/contextbuilder/networkdiscovery/service/SpringServiceImpl.java
@@ -390,17 +390,22 @@ public class SpringServiceImpl implements SpringService {
                         vm.setDataQuality(resource.getDataQuality());
                         if (null != resource.getAttributeList()) {
                             for (org.onap.sdnc.apps.pomba.networkdiscovery.datamodel.Attribute ndattribute : resource.getAttributeList()) {
-                                try {
-                                    Attribute attribute = new Attribute();
-                                    attribute.setName(Attribute.Name.valueOf(ndattribute.getName()));
-                                    attribute.setValue(ndattribute.getValue());
-                                    attribute.setDataQuality(ndattribute.getDataQuality());
-                                    vm.addAttribute(attribute);
-                                } catch (IllegalArgumentException ex) {
-                                    // The attribute Name passed back from Network Discovery is not in our enum
-                                    log.info("Atribute Name: " + ndattribute.getName() + " for Resource:" + resource.getName() + " Id:"  +resource.getId() +  " is invalid");
-                                }
-
+                                String ndattributeName = ndattribute.getName();
+                            	if (ndattributeName == "name") {
+                            		vm.setName(ndattribute.getValue());
+                            	}
+                            	else {
+	                                try {
+	                                    Attribute attribute = new Attribute();
+	                                    attribute.setName(Attribute.Name.valueOf(ndattributeName));
+	                                    attribute.setValue(ndattribute.getValue());
+	                                    attribute.setDataQuality(ndattribute.getDataQuality());
+	                                    vm.addAttribute(attribute);
+	                                } catch (IllegalArgumentException ex) {
+	                                    // The attribute Name passed back from Network Discovery is not in our enum
+	                                    log.info("Atribute Name: " + ndattributeName + " for Resource:" + resource.getName() + " Id:"  +resource.getId() +  " is invalid");
+	                                }
+                            	}
                             }
                         }
                     }
